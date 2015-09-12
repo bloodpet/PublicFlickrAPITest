95de27cbff9d19455db3f50a87d73b79f7640f71
Eman Calso <egcalso@gmail.com>
2015-09-12 13:10:28 +0800

    Add search form and search function that just log the search term

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,8 @@
 var app = angular.module('myApp', []);
 
+app.controller('SearchController', function($scope) {
+    $scope.searchTerm = '';
+    $scope.search = function() {
+        console.log('Start search for ' + $scope.searchTerm);
+    };
+})
