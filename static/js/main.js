4fbe8858e44fc81cadfdbff805c6287d91577ee0
Eman Calso <egcalso@gmail.com>
2015-09-12 13:47:52 +0800

    Add page title and change title depending on search

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,15 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', []).
+config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+        controller: 'SearchController',
+    })
+}]);
 
-app.controller('SearchController', function($scope) {
+app.controller('SearchController', function($scope, $http) {
     $scope.searchTerm = '';
+    $scope.pageTitle = 'All images';
     $scope.search = function() {
         console.log('Start search for ' + $scope.searchTerm);
+        $scope.pageTitle = 'Search for ' + $scope.searchTerm;
     };
 })
