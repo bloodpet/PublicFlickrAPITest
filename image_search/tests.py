77eca317215f9a53308f5eae6c651c2bb64bea59
Eman Calso <egcalso@gmail.com>
2015-09-12 19:16:11 +0800

    Add code to connect to redis

--- image_search/tests.py
+++ image_search/tests.py
@@ -1,5 +1,8 @@
 import json
 from django.test import TestCase
+from django.conf import settings
+from .kv_store import get_connection
+from .exceptions import MissingSettings
 
 
 class HomeTestCase(TestCase):
@@ -19,3 +22,19 @@ class SearchViewTest(TestCase):
         response = self.client.get('/search?search_term=')
         self.assertEqual(200, response.status_code)
         self.assertTrue('items' in response.data.keys())
+
+
+class RedisTest(TestCase):
+
+    def test_missing_connection(self):
+        conn_name = 'non-existent'
+        self.assertRaisesMessage(
+            MissingSettings, conn_name + ' connection not found',
+            get_connection, conn_name)
+
+    def test_valid_connection(self):
+        conn_name = 'default'
+        expected_port = settings.REDIS[conn_name]['port']
+        redis = get_connection(conn_name)
+        info = redis.info()
+        self.assertEqual(expected_port, info['tcp_port'])
