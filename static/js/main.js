b098d4a8120473b6013a3e56d11f83dddca07417
Eman Calso <egcalso@gmail.com>
2015-09-12 15:18:37 +0800

    Integrate search to front-end

--- static/js/main.js
+++ static/js/main.js
@@ -7,9 +7,19 @@ config(['$routeProvider', function($routeProvider) {
 }]);
 
 app.controller('SearchController', function($scope, $http) {
+    $scope.items = [];
     $scope.searchTerm = '';
     $scope.pageTitle = 'All images';
     $scope.search = function() {
         $scope.pageTitle = 'Search for ' + $scope.searchTerm;
+        $http({
+            method: 'GET',
+            url: '/search?search_term=' + $scope.searchTerm
+        }).success(function(data) {
+            $scope.items = data.items;
+        }).error(function(data, status) {
+            console.log(data);
+            console.log(status);
+        });
     };
 })
